65cfc017fb972eaefbc3ad9405860bbb1f99cf62
Jin Park <jinpark1504@gmail.com>
2024-05-21 13:37:01 +0900

    Multiple components of PickerView

--- UIKit-Basic2/ViewController.swift
+++ UIKit-Basic2/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
     private lazy var pickerView: UIPickerView = {
         let pickerView = UIPickerView()
         pickerView.delegate = self
@@ -17,6 +16,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return pickerView
     }()
     let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+    let options2 = ["옵션 1", "옵션 2", "옵션 3", "옵션 4", "옵션 5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,19 +30,32 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     // MARK: - UIPickerViewDataSouce
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
+        2
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        options.count
+        switch component {
+        case 0:
+            return options.count
+        case 1:
+            return options2.count
+        default:
+            return 0
+        }
     }
     
     // MARK: - UIPickerViewDelegate
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return options[row]
+        switch component {
+        case 0:
+            return options[row]
+        case 1:
+            return options2[row]
+        default:
+            return ""
+        }
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("selected row: \(row)")
+        print("selected component: \(component), row: \(row)")
     }
 }
 
