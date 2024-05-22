e4e97dc087c07a209f8351156e6b7de30e16c153
Jin Park <jinpark1504@gmail.com>
2024-05-22 09:35:48 +0900

    Tap Gesture

--- UIKit-Basic2/ViewController.swift
+++ UIKit-Basic2/ViewController.swift
@@ -10,5 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let rectangle = UIView()
+        rectangle.backgroundColor = .yellow
+        rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
+        rectangle.isUserInteractionEnabled = true
+        
+        view.addSubview(rectangle)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.numberOfTapsRequired = 2
+        rectangle.addGestureRecognizer((tapGesture))
+    }
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        if let view = sender.view {
+            view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+        }
     }
 }
